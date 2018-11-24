244c60537f2182a3dbcc0aab04b01d646d930a60
unknown <shitritg@post.bgu.ac.il>
2018-11-24 13:56:34 +0200

    check

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,6 @@ $(document).ready(function () {
             table.deleteRow(1);
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
-        var  temp = JSON.stringify(parsedCode);
-        console.log(temp);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
         showTable(parsedCode);
     });
